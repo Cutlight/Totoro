c78d0a01cbd1d677f79a1f58346ceb24910fc517
Cutlight <58677054+Cutlight@users.noreply.github.com>
2020-08-20 21:06:48 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,4 +1,4 @@
-local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Cutlight/Totoro/master/library.lua"))();
+local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Cutlight/Totoro/master/library"))();
 
 mytoggle = false
 
